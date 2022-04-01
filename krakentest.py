0facb6719ce1a2825c96f253d7d26705e5caec39
primkey7607 <psubramaniam@uchicago.edu>
2022-04-01 14:17:57 -0500

    kraken test not matching

--- krakentest.py
+++ krakentest.py
@@ -49,7 +49,7 @@ def linear_sanitycheck(dirname):
         X_test = jtestdf.loc[:, ~jtestdf.columns.isin(excols)]
         y_test = jtestdf['result']
         reg = LinearRegression().fit(X, y)
-        print(reg.coef_)
+        #print(reg.coef_)
         print(k + ': ' + str(reg.score(X_test, y_test)))
         
 linear_sanitycheck('../ardads/arda-datasets/datasets/kraken')
@@ -68,12 +68,21 @@ def use_kraken(dirname, tbl_name, limit=1, m=1):
             jks = row[1]
             ljk = jks.split('--')[0]
             rjk = jks.split('--')[1]
+            if rjk == 'index' and '109' in row[2]:
+                rjk = 'f109k'
+            elif rjk == 'index' and 'sensor4' in row[2]:
+                rjk = 'f4k'
             base_t[row[2]]['left_k'] = ljk
             base_t[row[2]]['right_k'] = rjk
     ind = 0
     for k in base_t:
         fname = os.path.join(dirname, k)
         newdf = pd.read_csv(fname, header=0)
+        if 'sensor4' in k:
+            newdf.rename(columns={'index' : 'f4k'}, inplace=True)
+        elif 'sensor109' in k:
+            newdf.rename(columns={'index' : 'f109k'}, inplace=True)
+            
         new_tbl = agg_dataset()
         #new_tbl.load(newdf, [], ["Provider City"], tbl_name + str(i))
         new_tbl.load(newdf, [], [base_t[k]['right_k']], k)
@@ -85,28 +94,29 @@ def use_kraken(dirname, tbl_name, limit=1, m=1):
         ind += 1
     
     buyer_ds = pd.read_csv(bname, header=0)
-    buyer_ds.rename(columns={'f4k' : '4index', 'f109k' : '109index'})
+    #buyer_ds.rename(columns={'f4k' : '4index', 'f109k' : '109index'})
     targ_val = {'fail' : 0, 'nofail' : 1}
     buyer_ds['result'] = [targ_val[item] for item in buyer_ds['result']]
 
     # train test split
     msk = split_mask(len(buyer_ds)) < 0.8
-    taxi_train = buyer_ds[msk].copy()
-    taxi_test = buyer_ds[~msk].copy()
+    b_train = buyer_ds[msk].copy()
+    b_test = buyer_ds[~msk].copy()
+    print("b_test cols: {}".format(b_test.columns))
 
-    taxi_train_data = agg_dataset()
-    taxi_train_data.load(taxi_train, [], [['event_id', '4index', '109index']], "krakenbase")
-    taxi_train_data.process_target('result')
-    taxi_train_data.to_numeric_and_impute_all()
-    taxi_train_data.remove_redundant_columns()
-    taxi_train_data.compute_agg()
+    b_train_data = agg_dataset()
+    b_train_data.load(b_train, [], ['event_id', 'f4k', 'f109k'], "krakenbase")
+    b_train_data.process_target('result')
+    b_train_data.to_numeric_and_impute_all()
+    b_train_data.remove_redundant_columns()
+    b_train_data.compute_agg()
 
-    taxi_test_data = agg_dataset()
-    taxi_test_data.load(taxi_test, [], [['event_id', '4index', '109index']], "krakenbase")
-    taxi_test_data.process_target("result")
-    taxi_test_data.to_numeric_and_impute_all()
-    taxi_test_data.remove_redundant_columns()
-    taxi_test_data.compute_agg()
+    b_test_data = agg_dataset()
+    b_test_data.load(b_test, [], ['event_id', 'f4k', 'f109k'], "krakenbase")
+    b_test_data.process_target("result")
+    b_test_data.to_numeric_and_impute_all()
+    b_test_data.remove_redundant_columns()
+    b_test_data.compute_agg()
 
     # find m best datasets to augment
     bought = set()
@@ -124,10 +134,11 @@ def use_kraken(dirname, tbl_name, limit=1, m=1):
             # check if current seller has been bought
             if sellerdata.name in bought:
                 continue
-            print("Sellerdata name: {}".format(sellerdata.name))
+            #print("Sellerdata name: {}".format(sellerdata.name))
+            #print("Dimension name: {}".format(dimension))
 
             # find the attributes and r2 of augmenting
-            cur_atts, final_r2 = select_features(taxi_train_data, taxi_test_data, sellerdata, dimension, 4, y)
+            cur_atts, final_r2 = select_features(b_train_data, b_test_data, sellerdata, dimension, 4, y)
             print(final_r2)
     #         cur_atts, final_r2 = select_features(gender_train, gender_test, sellerdata, dimension,10)
 
@@ -140,5 +151,5 @@ def use_kraken(dirname, tbl_name, limit=1, m=1):
 
         print(best_seller.name, best_seller_attrs, best_r2)
 
-#use_kraken('../ardads/arda-datasets/datasets/kraken', 'cur_tbl', limit=1, m=1)
+use_kraken('../ardads/arda-datasets/datasets/kraken', 'cur_tbl', limit=1, m=1)
 
